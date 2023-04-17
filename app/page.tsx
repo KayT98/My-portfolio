bc0ab3076ca1bc80004cd909ee26cc8c61a75b65
KayT98 <khanhtruong109@yahoo.com>
2023-04-16 22:17:29 -0500

    fixed lists and introduction, added bolder font

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,10 @@ export default function Home() {
         <h4 className="text-2xl font-extrabold"><a href="#">Kay Truong</a></h4>
         <nav>
           <ul>
-            <a style={{ marginRight: '50px' }} href="#top">About Me</a>
-            <a style={{ marginRight: '50px' }} href='#skills'>Skills</a>
-            <a style={{ marginRight: '50px' }} href='#projects'>Projects</a>
-            <a style={{ marginRight: '50px' }} href='#contact'>Contact</a>
+            <a style={{ marginRight: '50px', fontWeight:'bold' }} href="#top">About Me</a>
+            <a style={{ marginRight: '50px', fontWeight:'bold' }} href='#skills'>Skills</a>
+            <a style={{ marginRight: '50px', fontWeight:'bold' }} href='#projects'>Projects</a>
+            <a style={{ marginRight: '50px', fontWeight:'bold' }} href='#contact'>Contact</a>
           </ul>
         </nav>
       </div>
@@ -32,13 +32,15 @@ export default function Home() {
 
     <div id="top">
         <h1 className="text-4xl font-bold text-white"> &nbsp;Hello! My name is Kay Truong</h1>
-          <h2 className="text-3xl font-bold">I&apos;m a Front-End Web Developer at Seminaut Inc</h2>
-      <p className="text-center">I graduated from Northeastern State University in Oklahoma, Spring 2022. I was born and raised in Vietnam
-        for 14 years before moving to the US. I always curious on how websites were made and after seeing my uncle working on
-        his website project, I started to like website development. I&apos;m a hardworking and passionate job seeker with strong organizational skills,
-        with experience in HTML5 & CSS3, JavaScript, Front End Development and UI Design, I&apos;m currently learning and working on project with NextJS 13, Tailwind, TypeScript and React. Highly motivated and bilingual in Vietnamese and English.
-        Ready and willing to learn anything new to help the team achieve company goals. Superior work ethic and good teamwork, problem solving and organizational skills. Wanting to gain more experience in an
-        environment that encourages growth.
+          <h2 className="text-3xl font-bold">I&apos;m a Frontend Web Developer at Seminaut Inc</h2>
+      <p className="text-center font-bold">I graduated from Northeastern State University in Oklahoma, Spring 2022. I&apos;m a hardworking and passionate job seeker with strong organizational skills, with experience in HTML5 & CSS3, JavaScript(ES6), Front End Development and UI Design. Currently, I am working as an intern at Seminaut Inc, 
+      where I am gaining valuable experience in software engineering and website development. In my spare time, I am currently focusing on learning new programming languages such as Python, C# and Java, as well as exploring the possibilities of Next.js. 
+      I believe that these skills will help me to pursue better opportunities in the future and stay ahead in this constantly evolving industry. 
+      Highly motivated and bilingual in Vietnamese and English. Ready and willing to learn anything new to help the team achieve company goals. Superior work ethic and good teamwork, problem solving and organizational skills. 
+      I am deeply passionate and excited to see where my passion for software engineering and website development will take me in the future, 
+      and I look forward to making a positive impact in the field.
+
+
       </p>
         
           <div className={styles.downloadBtn}>
@@ -47,19 +49,19 @@ export default function Home() {
 
               <h1 id="skills" className="text-3xl font-bold text-white">Skills</h1>
           <div className={styles.skills}>  
-            <ul>      
-              <li>-Programming languages/Frameworks/Libraries: HTML5/HTML/CSS3/CSS, JavaScript(ES6), SQL/MYSQL, Bootstrap5, React, NextJS 13, Tailwind, XML, JSON, JQuery, C# (basic knowledge) </li>
-              <li>-Software: Figma, Microsoft Office, Microsoft SQL Server, Visual Studio Code, Git Bash, Github, ClickUp</li>
-              <li>-Technical skills: IT (basic knowledge), Computer Building.</li>
-              <li>-Soft skills: Time Management, Creative Thinking, Teamworking, Responsibility, Ability to work under pressure, Flexibility, Problem Solving, Excellent Verbal & Written Communication,
-                Determining the Structure & Design of Web Pages, Ensuring Web Design is Optimized for Mobile Devices.</li>
+            <ul className='font-bold'>     
+              <li className='p-1'>-Working knowledge: HTML5, CSS3, Javascript(ES6), React, Git, TailwindCSS, VS Code, UI Development, Frontend Development, Responsive Web Development, Github, Clickup.</li>
+              <li className='p-1'>-Familiar with: SQL/MYSQL, Bootstrap5, NextJS 13, XML, JSON, JQuery, C#, Python, OOP, WordPress.</li>
+              <li className='p-1'>-Software: Figma, Microsoft Office, Microsoft SQL Server, Visual Studio Code, Git Bash, Github, ClickUp</li>
+              <li className='p-1'>-Soft skills: Time Management, Creative Thinking, Teamwork, Responsibility, Ability to work under pressure and independently, Flexibility, Problem Solving, Excellent Verbal & Written Communication, Work Ethic, 
+              Attention To Detail, Adaptability.</li>
             </ul>     
           </div>
 
           <h1 id="projects" className="text-3xl font-bold text-white">Guild Gaming/Seminaut Inc Tasks</h1>
           <div className={styles.project}> 
           <ul>
-              <li className={styles.li}>Guild Gaming UI Task - Display 5 Badges and Display All Badges When Button Is Clicked - Written in React & CSS3 (Mobile UI)
+              <li className={styles.li}>Guild Gaming UI Task - Display 5 Badges and Display All Badges When Button Is Clicked - Written in React & TailwindCSS (Mobile UI)
               <img src={'../5badges.png'} className={styles.img} alt='guildgaming' width={400} height={400}></img>       
 
               <video className={styles.img} width={400} height={400} controls autoPlay>
@@ -71,7 +73,7 @@ export default function Home() {
               <img src={'../featuredevents.png'} className={styles.img} alt='guildgaming' width={200} height={150}></img>              
               </li>
 
-              <li className={styles.li}>Guild Gaming UI Task - User Activity Post and Post Modal When Button Clicked - Written in React (Desktop UI) 
+              <li className={styles.li}>Guild Gaming UI Task - User Activity Post and Post Modal When Button Clicked - Written in React & TailwindCSS (Desktop UI) 
               <img src={'../userpost.png'} className={styles.img} alt='guildgaming' width={550} height={500}></img>
               <img src={'../userpostmodal.png'} className={styles.img} alt='guildgaming' width={350} height={300}></img>    
               </li>
@@ -83,7 +85,7 @@ export default function Home() {
               <h1 id="projects" className="text-3xl font-bold text-white">Personal Projects</h1>
           <div className={styles.project}> 
               <ul>
-              <li className={styles.li}><Link target="_blank" href='https://kayt98.github.io/pet-pics-api/'>Pet Pics API Fetch - Written in React <FontAwesomeIcon icon={faLink} /></Link></li>
+              <li className={styles.li}><Link target="_blank" href='https://kayt98.github.io/pet-pics-api/'>Pet Pics API Fetch - Written in React & CSS3 <FontAwesomeIcon icon={faLink} /></Link></li>
               <img src={'../api.png'} className={styles.img} alt='api' width={350} height={300}></img>
               
               <li className={styles.li}><Link target="_blank" href='https://kayt98.github.io/MorsecodeTranslator/'>Morse Code Translator - Written in HTML5, CSS3 and JavaScript <FontAwesomeIcon icon={faLink} /></Link></li>
